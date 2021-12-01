02969ac0ee3bc867f8ed0a39331dd89e23dcdc32
sntmsc <santiago.hamber@gmail.com>
2021-12-01 09:53:31 -0300

    add menu

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
 import React from 'react'
 import './App.css'
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Stack , Flex, Text } from '@chakra-ui/react'
+
+const Menu = () => {
+  const CustomText = ({children}) => {
+    return(
+      <Text
+      fontSize='xl'
+      fontFamily='Source Code Pro, monospace'
+      fontWeight='bold'>
+        {children}
+      </Text>
+    )
+  }
+return(
+    <Stack
+    bg='red'
+    w='100%'
+    h='3em'
+    direction='row'
+    justify='end'
+    align='center'
+    spacing='1em'
+    pr='1em'>
+      <CustomText>
+        Home 
+      </CustomText>
+      <CustomText>
+        Sobre mi 
+      </CustomText>
+      <CustomText>
+        Contactos
+      </CustomText>
+    </Stack>
+  )
+}
 
 function App() {
   return (
     <Flex className="App" 
     w='100%' 
     h='100%' 
-    bg='red'
-    align='center'>
+    align='center'
+    direction='column'>
+     <Menu/>
       <Flex 
       w='100%'
-      h='40%' 
+      h='60%' 
       direction='column'
       justify='center'
       align='center'
